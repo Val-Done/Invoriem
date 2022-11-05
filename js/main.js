ebca1d43aa6ec43d9b45a3246d7c6c760a79c60d
Val-Done <72663081+Val-Done@users.noreply.github.com>
2022-11-05 23:18:28 +0300

    Last version desktop and mobile

--- js/main.js
+++ js/main.js
@@ -1,3 +1,68 @@
-$(document).ready(function(){
-  $(".owl-carousel").owlCarousel();
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel(
+    {
+      items: 3,
+      loop: true,
+      responsive: {
+        // breakpoints from 0 to up
+        0: {
+          items: 2,
+          margin: 32,
+          dots: false,
+          loop: true,
+        },
+        // breakpoints from 480 up
+        601: {
+          items: 3,
+          dots: false,
+          margin: 32,
+          loop: true,
+        },
+        // breakpoints from 1000 up
+        1001: {
+          items: 3,
+          margin: 48,
+          dots: false,
+          loop: true,
+        },
+      },
+    }
+    //
+  );
 });
+
+/*video */
+const videoBtn = document.querySelector("#video-btn");
+
+const videoPicture = document.querySelector(".video__picture");
+
+const videoWrapper = document.querySelector(".video");
+
+const video = document.querySelector("#video-file");
+
+videoWrapper.addEventListener("click", function () {
+  if (video.paused) {
+    videoPicture.classList.add("hidden");
+    videoWrapper.classList.remove("video-overlay");
+    videoBtn.classList.add("none");
+    video.play();
+  } else {
+    videoWrapper.classList.add("video-overlay");
+    videoBtn.classList.remove("none");
+    video.pause();
+  }
+});
+
+// * Mobile Nav*
+const openNavbtn = document.querySelector("#openMobileNav");
+const closeNavbtn = document.querySelector("#closeMobileNav");
+const mobileNav = document.querySelector("#mobileNav");
+
+openNavbtn.onclick = function () {
+  mobileNav.classList.remove("none");
+  document.body.classList.add("no-scroll");
+};
+closeNavbtn.onclick = function () {
+  mobileNav.classList.add("none");
+  document.body.classList.remove("no-scroll");
+};
